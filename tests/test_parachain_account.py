0bb3b60c8dacc94bf989f703d7ca3a555af496f0
Oladiy <bd3396121@gmail.com>
2021-08-11 21:27:58 +0300

    Fix para_addr comparison

--- tests/test_parachain_account.py
+++ tests/test_parachain_account.py
@@ -6,4 +6,4 @@ def test_show_correct_parachain_address():
     ss58_format = 2
     para_addr = 'F7fq1jSNVTPfJmaHaXCMtatT1EZefCUsa7rRiQVNR5efcah'
 
-    assert para_addr == SubstrateInterfaceUtils.get_parachain_address(para_id, ss58_format)
+    assert para_addr == SubstrateInterfaceUtils.get_parachain_address(para_id, ss58_format).ss58_address
