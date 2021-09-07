90a8ab3c77f8472173bb7e1a372a8791b60d5bb1
Oladiy <bd3396121@gmail.com>
2021-09-07 04:02:22 +0300

    Update test_parachain_account test

--- tests/test_parachain_account.py
+++ tests/test_parachain_account.py
@@ -1,9 +1,12 @@
 from oracleservice.substrate_interface_utils import SubstrateInterfaceUtils
+from substrateinterface import Keypair
 
 
 def test_show_correct_parachain_address():
     para_id = 1000
     ss58_format = 2
-    para_addr = 'F7fq1jSNVTPfJmaHaXCMtatT1EZefCUsa7rRiQVNR5efcah'
 
-    assert para_addr == SubstrateInterfaceUtils.get_parachain_address(para_id, ss58_format).ss58_address
+    para_addr_expected = 'F7fq1jSNVTPfJmaHaXCMtatT1EZefCUsa7rRiQVNR5efcah'
+    para_addr_actual = Keypair(public_key=SubstrateInterfaceUtils.get_parachain_address(para_id), ss58_format=ss58_format)
+    
+    assert para_addr_expected == para_addr_actual.ss58_address
